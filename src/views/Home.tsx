e952fd961814683ed13dbd104b0b249a2e3aa4f7
Juan Pérez Gago <juan.perez@interacso.com>
2024-01-07 20:24:06 +0100

    home-view: create Home view component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,24 @@
+import { Logo } from '../components/icons'
+
 interface Props {
   onPlay: () => void
 }
 
 const Home: React.FC<Props> = ({ onPlay }) => {
-  return <main>
-    <h1>Home</h1>
-    <button onClick={onPlay}>Comenzar</button>
-  </main>
+  return (
+    <main className='bg-blue-uelz min-h-screen grid place-content-center text-white text-center'>
+      <section>
+        <Logo/>
+        <h1 className='mb-[60px]'>Test de memoria</h1>
+        <button
+          className='bg-green-clear-uelz hover:bg-green-dark-uelz active:bg-green-dark-uelz active:ring active:ring-green-clear-uelz rounded-md py-3 px-7 text-xl font-semibold leading-7'
+          onClick={onPlay}
+        >
+          Comenzar
+        </button>
+      </section>
+    </main>
+  )
 }
 
 export default Home
