3d2bca5f32c600d3fc39ce78cfd5a3ac167da6b8
Juan Pérez Gago <juan.perez@interacso.com>
2024-01-08 02:44:43 +0100

    game-ui: show cards in Game view

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -1,5 +1,18 @@
+import { Card } from '../components'
+import { useGame } from '../hooks'
+
 const Game: React.FC = () => {
-  return <h1>Game</h1>
+  const [cardsInfo] = useGame()
+
+  return (
+    <main className='min-h-screen bg-green-soft-uelz grid place-content-center'>
+      <section className='grid grid-cols-6 gap-[36px]'>
+        {cardsInfo.map(cardInfo => (
+          <Card key={cardInfo.id} cardInfo={cardInfo}/>
+        ))}
+      </section>
+    </main>
+  )
 }
 
 export default Game
