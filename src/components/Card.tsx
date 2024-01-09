df58d268eddadc1e5903f9590ac57fb45110f18e
Juan Pérez Gago <juan.perez@interacso.com>
2024-01-09 19:25:42 +0100

    flip-card-animation: create flip animation to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,10 +8,16 @@ interface Props {
 
 const Card: React.FC<Props> = ({ cardInfo, onBackCardPress }) => {
   const { id, emoji, isVisible } = cardInfo
+  const classNameBase = 'relative size-[124px] [transform-style:preserve-3d] duration-700'
 
-  return isVisible
-    ? <FrontCard emoji={emoji}/>
-    : <BackCard id={id} onBackCardPress={onBackCardPress}/>
+  return (
+    <div className='[perspective:400px]'>
+      <div className={isVisible ? classNameBase + ' [transform:rotateY(180deg)]' : classNameBase}>
+        <FrontCard emoji={emoji}/>
+        <BackCard id={id} onBackCardPress={onBackCardPress}/>
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- src/components/FrontCard.tsx
+++ src/components/FrontCard.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const FrontCard: React.FC<Props> = ({ emoji }) => (
-  <div className='card bg-yellow-soft-uelz'>
+  <div className='card bg-yellow-soft-uelz [transform:rotateY(180deg)]'>
     <img src={emoji} alt='emoji' className='size-[68px]'/>
   </div>
 )
